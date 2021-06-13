9231f8ada92ea8d86bdfde73a1c3fb32beeb78a8
Finn James <finn.james@sas.com>
2021-06-14 00:25:21 +0200

    sliiiightly less basic svelte app

---
+++ src/pages/Index.svelte
@@ -0,0 +1,10 @@
+<h1>Hey!</h1>
+<p>
+	My name is Finn. How’s it going? I like drawing, reading, photography, and
+	music. I am a Computer Science major at UNC Chapel Hill and a Python and
+	TypeScript developer for the <a href="https://skynet.unc.edu/"
+		>Skynet robotic telescope network</a
+	>.
+</p>
+
+<Contact />
