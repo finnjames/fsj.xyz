01f3bb6c713ce2b1323d6b009b334f34078f8520
Finn <finn.s.james@gmail.com>
2021-07-16 15:31:36 -0400

    lazy loading on portfolio

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -2,14 +2,14 @@
 
 <div id="portfolio">
   <h1 id="photography" class="no-hash">Photography</h1>
-  <img alt="Eszter 1" src="/art/p-e2.jpeg" />
-  <img alt="Thomas" src="/art/p-t1.jpeg" />
-  <img alt="Hannah" src="/art/p-h1.jpeg" />
-  <img alt="Eszter 2" src="/art/p-e1.jpeg" />
+  <img loading="lazy" alt="Eszter 1" src="/art/p-e2.jpeg" />
+  <img loading="lazy" alt="Thomas" src="/art/p-t1.jpeg" />
+  <img loading="lazy" alt="Hannah" src="/art/p-h1.jpeg" />
+  <img loading="lazy" alt="Eszter 2" src="/art/p-e1.jpeg" />
   <h1 id="drawing" class="no-hash">Drawing</h1>
-  <img alt="Charcoal drawing of my desk" class="image" src="/art/a-c1.jpeg" />
-  <img alt="Ink drawing in the style of Mucha" class="image" src="/art/a-i1.jpeg" />
-  <img alt="Charcoal drawings with a backlight" class="image" src="/art/a-c2.jpeg" />
+  <img loading="lazy" alt="Charcoal drawing of my desk" class="image" src="/art/a-c1.jpeg" />
+  <img loading="lazy" alt="Ink drawing in the style of Mucha" class="image" src="/art/a-i1.jpeg" />
+  <img loading="lazy" alt="Charcoal drawings with a backlight" class="image" src="/art/a-c2.jpeg" />
 </div>
 
 <style lang="scss">
