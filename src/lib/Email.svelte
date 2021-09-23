3c6d9e3efea538dc4a1269a2e72e4be10d8b9147
Finn <finn.s.james@gmail.com>
2021-09-22 22:00:34 -0400

    add a space

--- src/lib/Email.svelte
+++ src/lib/Email.svelte
@@ -8,7 +8,7 @@
   };
 
   function hex2a(hexx) {
-    var hex = hexx.toString(); //force conversion
+    var hex = hexx.toString(); // force conversion
     var str = "";
     for (var i = 0; i < hex.length; i += 2) {
       str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
